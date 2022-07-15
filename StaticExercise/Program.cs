17da772b4b6410c88a7fa3dbea43d77527b63e6a
cpheighter <cheighterjr@gmail.com>
2022-07-15 15:01:34 -0400

    init

---
+++ StaticExercise/Program.cs
@@ -0,0 +1,7 @@
+﻿using StaticExercise;
+
+Console.WriteLine("Fahrenheit To Celsius");
+Console.WriteLine(TempConverter.FahrenheitToCelsius(32));
+Console.WriteLine("-------------------");
+Console.WriteLine("Celsius To Fahrenheit");
+Console.WriteLine(TempConverter.CelsiusToFahrenheit(100));
